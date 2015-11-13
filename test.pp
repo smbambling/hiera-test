0a54cdd82f8cfa1a0def9401bf6bdeeffc8a227c
Steven Bambling <smbambling@gmail.com>
2015-11-13 09:23:15 -0500

    adding commented out logic to really manage resources

--- test.pp
+++ test.pp
@@ -5,6 +5,7 @@ notice ( $managed_groups )
 $managed_groups.each | $group | {
   notice ( "Processing $group" )
   $g = hiera_hash(accounts::groups).filter | $key, $val | { $key == $group }
+  # create_resources('group', $g)
   notice ( $g )
 }
 
@@ -13,6 +14,7 @@ $managed_users = hiera_array(managed_users)
 $managed_users.each | $user | {
   notice ( "Processing $user" )
   $u = hiera_hash(accounts::users).filter | $key, $val | { $key == $user }
+  # create_resources(pe_accounts::user, $u)
   notice ( $u )
 }
 
