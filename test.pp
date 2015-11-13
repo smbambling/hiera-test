6c0a1f092e1bcfa75a739796ab1a2d64510e30d2
Steven Bambling <smbambling@gmail.com>
2015-11-13 05:44:36 -0500

    first commit

---
+++ test.pp
@@ -0,0 +1,11 @@
+$managed_groups = hiera_array(managed_groups)
+
+notice ( $managed_groups )
+
+$managed_users = hiera_array(managed_users)
+
+$managed_users.each | $user | {
+  $u = hiera_hash(accounts::users).filter | $key, $val | { $key == $user }
+  notice ( $u )
+}
+
